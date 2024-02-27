9dc04280bf93896b2c15e481cf4a6d7eef784ab8
gethubbyraj <rajkumar14396@gmail.com>
2024-02-27 12:27:47 +0530

    sample project

---
+++ azurefunctions/src/main/java/com/test/azure/azurefunctions/AzurefunctionsApplication.java
@@ -0,0 +1,13 @@
+package com.test.azure.azurefunctions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzurefunctionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzurefunctionsApplication.class, args);
+	}
+
+}
